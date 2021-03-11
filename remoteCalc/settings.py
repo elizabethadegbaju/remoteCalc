2e3bacea75f5c9f4c4465a91a2433c87254d0c85
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2021-03-11 01:10:05 +0100

    chore: setup documents

--- remoteCalc/settings.py
+++ remoteCalc/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q_-mh$-q^#)tkow#&q3_*$8j7!i5se$0s4qgew2kgf#9fj&&hv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
